20b27e7fe14f90477ab85c6b4a1d753d3bed88a3
Tom Mendy <tom.mendy@epitech.eu>
2024-11-19 18:34:53 +0100

    [ADD] jenkins job

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -1,4 +1,105 @@
 // def folderName = "Tools"
+folder('Whanos base images') {
+    description('Folder for miscellaneous tools.')
+}
+
+
+
+job('Whanos base images/whanos-c') {
+    description('build the c base image')
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('docker build -t whanos-c .')
+    }
+}
+
+job('Whanos base images/whanos-java') {
+    description('build the java base image')
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('docker build -t whanos-java .')
+    }
+}
+
+
+
+
+
+job('Whanos base images/whanos-javascript') {
+    description('build the javascript base image')
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('docker build -t whanos-javascript .')
+    }
+}
+
+job('Whanos base images/whanos-python') {
+    description('build the python base image')
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('docker build -t whanos-python .')
+    }
+}
+
+job('Whanos base images/whanos-befunge') {
+    description('build the befunge base image')
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('docker build -t whanos-befunge .')
+    }
+}
+
+
+job('Whanos base images/Build all base images') {
+    description('Build all base images')
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+}
+
+
+folder('Projects') {
+    description('Folder for miscellaneous tools.')
+}
+
+
+
+
+
 folder('Tools') {
     description('Folder for miscellaneous tools.')
 }
