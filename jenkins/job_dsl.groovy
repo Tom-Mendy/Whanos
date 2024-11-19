1200b99073ef4b089130a5a88a108484568e4d50
Tom Mendy <tom.mendy@epitech.eu>
2024-11-19 09:51:52 +0100

    [ADD] jenkins

---
+++ jenkins/job_dsl.groovy
@@ -0,0 +1,60 @@
+// def folderName = "Tools"
+folder('Tools') {
+    description('Folder for miscellaneous tools.')
+}
+
+job('Tools/clone-repository') {
+    description('Job to clone a repository')
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('git clone $GIT_REPOSITORY_URL')
+    }
+}
+
+job('Tools/SEED') {
+    description('Seed job to create other jobs')
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
+    }
+    steps {
+        dsl {
+            scriptText = '''
+job(DISPLAY_NAME) {
+    scm {
+        git {
+            remote {
+                github(GITHUB_NAME)
+            }
+        }
+    }
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('make fclean')
+        shell('make')
+        shell('make tests_run')
+        shell('make clean')
+    }
+    triggers {
+        scm('* * * * *')
+    }
+}
+'''
+        }
+    }
+}
