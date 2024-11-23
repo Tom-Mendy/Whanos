ceb4301a7abb3f25973a56f95ed708649c4b889b
Tom Mendy <tom.mendy@epitech.eu>
2024-11-23 12:09:02 +0100

    [ADD] trigger image build

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -1,161 +1,49 @@
-// def folderName = "Tools"
 folder('Whanos base images') {
-    description('Folder for miscellaneous tools.')
+    description('Folder for building Whanos base images.')
 }
-
-
-
-job('Whanos base images/whanos-c') {
-    description('build the c base image')
-    wrappers {
-        preBuildCleanup { // Clean before build
-            includePattern('**/target/**')
-            deleteDirectories()
-            cleanupParameter('CLEANUP')
-        }
-    }
-    steps {
-        shell('docker build -t whanos-c .')
-    }
-}
-
-job('Whanos base images/whanos-java') {
-    description('build the java base image')
-    wrappers {
-        preBuildCleanup { // Clean before build
-            includePattern('**/target/**')
-            deleteDirectories()
-            cleanupParameter('CLEANUP')
-        }
-    }
-    steps {
-        shell('docker build -t whanos-java .')
-    }
-}
-
-
-
-
-
-job('Whanos base images/whanos-javascript') {
-    description('build the javascript base image')
-    wrappers {
-        preBuildCleanup { // Clean before build
-            includePattern('**/target/**')
-            deleteDirectories()
-            cleanupParameter('CLEANUP')
-        }
-    }
-    steps {
-        shell('docker build -t whanos-javascript .')
-    }
-}
-
-job('Whanos base images/whanos-python') {
-    description('build the python base image')
-    wrappers {
-        preBuildCleanup { // Clean before build
-            includePattern('**/target/**')
-            deleteDirectories()
-            cleanupParameter('CLEANUP')
-        }
-    }
-    steps {
-        shell('docker build -t whanos-python .')
-    }
-}
-
-job('Whanos base images/whanos-befunge') {
-    description('build the befunge base image')
-    wrappers {
-        preBuildCleanup { // Clean before build
-            includePattern('**/target/**')
-            deleteDirectories()
-            cleanupParameter('CLEANUP')
-        }
-    }
-    steps {
-        shell('docker build -t whanos-befunge .')
-    }
-}
-
-
-job('Whanos base images/Build all base images') {
-    description('Build all base images')
-    wrappers {
-        preBuildCleanup { // Clean before build
-            includePattern('**/target/**')
-            deleteDirectories()
-            cleanupParameter('CLEANUP')
-        }
-    }
-}
-
 
 folder('Projects') {
-    description('Folder for miscellaneous tools.')
+    description('Folder containing linked project jobs.')
 }
 
+def baseImages = ['whanos-c', 'whanos-java', 'whanos-javascript', 'whanos-python', 'whanos-befunge']
 
-
-
-
-folder('Tools') {
-    description('Folder for miscellaneous tools.')
-}
-
-job('Tools/clone-repository') {
-    description('Job to clone a repository')
-    parameters {
-        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
-    }
-    wrappers {
-        preBuildCleanup { // Clean before build
-            includePattern('**/target/**')
-            deleteDirectories()
-            cleanupParameter('CLEANUP')
-        }
-    }
-    steps {
-        shell('git clone $GIT_REPOSITORY_URL')
-    }
-}
-
-job('Tools/SEED') {
-    description('Seed job to create other jobs')
-    parameters {
-        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
-        stringParam('DISPLAY_NAME', '', 'Display name for the job')
-    }
-    steps {
-        dsl {
-            scriptText = '''
-job(DISPLAY_NAME) {
-    scm {
-        git {
-            remote {
-                github(GITHUB_NAME)
+baseImages.each { image ->
+    job("Whanos base images/${image}") {
+        description("Build the ${image} base image")
+        wrappers {
+            preBuildCleanup {
+                includePattern('**/target/**')
+                deleteDirectories()
+                cleanupParameter('CLEANUP')
             }
         }
-    }
-    wrappers {
-        preBuildCleanup { // Clean before build
-            includePattern('**/target/**')
-            deleteDirectories()
-            cleanupParameter('CLEANUP')
+        steps {
+            shell("docker build -t ${image} .")
         }
     }
-    steps {
-        shell('make fclean')
-        shell('make')
-        shell('make tests_run')
-        shell('make clean')
-    }
-    triggers {
-        scm('* * * * *')
-    }
 }
-'''
+
+
+pipelineJob('Whanos base images/Build all base images') {
+    definition {
+        cps {
+            script("""
+            pipeline {
+                agent any
+                stages {
+                    stage('Trigger All') {
+                        steps {
+                            build job: 'Whanos base images/whanos-c'
+                            build job: 'Whanos base images/whanos-java'
+                            build job: 'Whanos base images/whanos-javascript'
+                            build job: 'Whanos base images/whanos-python'
+                            build job: 'Whanos base images/whanos-befunge'
+                        }
+                    }
+                }
+            }
+            """)
         }
     }
 }
